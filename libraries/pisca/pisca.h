57071903e306be87817c4dc2bc1ff0d24f64dc6b
Vitor Oliveira Franco <31902277+kioryOF@users.noreply.github.com>
2018-05-18 16:27:28 -0300

    Add files via upload

--- libraries/pisca/pisca.h
+++ libraries/pisca/pisca.h
@@ -15,15 +15,15 @@ void desligar(){
     digitalWrite(13,LOW);
 }
 void informacoesMontagem(){
-Serial.write("*****Informacoes Montagem*****");
-Serial.write("O led se encotra na porta 13;");
-Serial.write("*****");
+Serial.println("*****Informacoes Montagem*****");
+Serial.println("O led se encotra na porta 13;");
+Serial.println("*****");
 }
 void informacoesExercicio(){
-Serial.write("*****Informacoes Exercicio*****");
-Serial.write("Exercicio na introducao a funcoes simplificadas,ambientacao e a plataforma Arduino, consiste no aluno 
+Serial.println("*****Informacoes Exercicio*****");
+Serial.println("Exercicio na introducao a funcoes simplificadas,ambientacao e a plataforma Arduino, consiste no aluno 
 \n  acender, desligar e piscar um led;");
-Serial.write("*****");
+Serial.println("*****");
 
 }
 
