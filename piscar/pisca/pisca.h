c148404a8cf86d98b2db20a6f26872c8368b0770
Vitor Oliveira Franco <31902277+kioryOF@users.noreply.github.com>
2018-05-24 14:55:27 -0300

    Add files via upload

--- piscar/pisca/pisca.h
+++ piscar/pisca/pisca.h
@@ -1,7 +1,7 @@
 #ifndef PISCA_H_INCLUDED
 #define PISCA_H_INCLUDED
+#define main setup
 #include <Arduino.h>
-
 
 void inicializar(){
     pinMode(13,OUTPUT);
--- piscar/piscar.ino
+++ piscar/piscar.ino
@@ -1,4 +1,21 @@
 #include <pisca.h>
+<<<<<<< HEAD
+
+void main() {
+  inicializar();
+  while (true) {
+
+    ligar();
+    delay(1000);
+    desligar();
+    delay(1000);
+
+  }
+  return 2;
+}
+
+
+=======
 void setup() {
   inicializar();
   while(true){
@@ -13,3 +30,4 @@ void setup() {
 
 void loop(){
   }
+>>>>>>> e4cffbb94d74ea6a99cf102b361a202f2bba2600
