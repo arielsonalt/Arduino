08bde8f00c40aa0f21cc792063a1b07d6ee0b0d9
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2018-08-29 10:34:20 -0300

    atualização

--- cidade-maluca/cidade-maluca.ino/cidade-maluca.ino.ino
+++ cidade-maluca/cidade-maluca.ino/cidade-maluca.ino.ino
@@ -5,7 +5,37 @@ int verde1 = 4;
 int verde2 = 8;
 int vermelho1 = 2;
 int vermelho2 = 6;
+int length = 26;
+char notes[] = "eeeeeeegcde fffffeeeeddedg";
+int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+int tempo = 300;
+boolean ledOnOff = true;
 int speaker = 10;
+
+
+
+
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speaker, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speaker, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
 
 void setup() {
   pinMode(verde1, OUTPUT); 
@@ -48,13 +78,30 @@ void loop() {
   }
 
   if(numAleatorio1 == 0 && numAleatorio2 == numAleatorio1){
-    digitalWrite(speaker, HIGH);
     delay(1000);
-    digitalWrite(verde1, LOW);
+     digitalWrite(verde1, LOW);
     digitalWrite(verde2, LOW);
     digitalWrite(vermelho1, HIGH);
     digitalWrite(vermelho2, HIGH);
-    delay(3000);
+    
+ for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+      
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+      
+    }
+
+    // pause between notes
+    delay(tempo / 2); 
+  }
+
+ 
+              noTone(speaker);
+
+    
+   
     digitalWrite(speaker, LOW);
   }
   delay(2000);
